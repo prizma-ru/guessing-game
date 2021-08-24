156e661d8b36b48bda908392204066f803890ad6
prizma-ru <dmitry.glotov@gmail.com>
2021-08-24 09:34:50 +0300

    feat: add code task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.midle = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
+        this.midle = Math.ceil((this.max + this.min) / 2);
+        return this.midle;
 
     }
 
     lower() {
-
+        this.min = this.midle;
     }
 
     greater() {
-
+        this.max = this.midle;
     }
 }
 
